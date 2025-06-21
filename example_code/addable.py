3a59854d57fcef89d2aa2784aa73880df4f8f0fe
LorenzoDG2 <117301895+LorenzoDG2@users.noreply.github.com>
2025-06-21 13:24:51 +0100

    oop

--- example_code/addable.py
+++ example_code/addable.py
@@ -1,19 +1,19 @@
 from abc import ABC, abstractmethod
 
 
-class Addable(ABC):
+class Addable(ABC):  # class of classes which has an add method
 
     @abstractmethod
     def __add__(self, other):
-        return NotImplemented
+        return NotImplemented  # add that doesnt know how to add
 
     @classmethod
-    def __subclasshook__(cls, C):
-        if cls is not Addable:
-            return NotImplemented
-        for B in C.__mro__:
-            if "__add__" in B.__dict__:
-                if B.__dict__["__add__"] is None:
+    def __subclasshook__(cls, C):  # declare ourselves to be a parent class of other classes, cls = our class
+        if cls is not Addable:  # boiler plate
+            return NotImplemented  # if subclasshook called on a subclass, no say on whether smth is a subclass of the subclass
+        for B in C.__mro__:  # for subclass in C's method-resolution-order, 
+            if "__add__" in B.__dict__:  # dict of a class contains all attributes and methods that are live on that class
+                if B.__dict__["__add__"] is None:  # B doesn't implement add, add is not allowed
                     return NotImplemented
-                return True
-        return NotImplemented
+                return True  # found add somewhere
+        return NotImplemented  # went through method resolution order and haven't found an add
